406cb6f35a27625b66634ccc64daf518473a4e28
Ayyub2311 <ayyubkhondjumabaev@gmail.com>
2026-07-09 14:43:29 +0500

    env added

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
-  method: 'GET',
+  method: 'GET', 
   headers: {
     accept: 'application/json',
     Authorization: `Bearer ${API_KEY}`
